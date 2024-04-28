b2ffac85daca08d64545104f79cefc6e86acb7f2
dae-ne <rafalcz29@gmail.com>
2024-04-29 01:50:31 +0200

    Add basic notifications functionality

---
+++ src/WeightTracker.Functions/Functions/SendReminders.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using WeightTracker.Functions.Notifications;
+
+namespace WeightTracker.Functions.Functions;
+
+public class SendReminders(
+    INotificationService notificationService,
+    ILogger<SendReminders> logger)
+{
+    private const string FunctionName = "send-reminders-func";
+
+    [Function(FunctionName)]
+    public async Task RunAsync([TimerTrigger("0 */5 * * * *", RunOnStartup = true)] TimerInfo timer)
+    {
+        logger.LogInformation("'{functionName}' started at: {time}", FunctionName, DateTimeOffset.Now);
+
+        await notificationService.SendReminderAsync(string.Empty);
+
+        logger.LogInformation("'{functionName}' completed at: {time}", FunctionName, DateTimeOffset.Now);
+
+        if (timer.ScheduleStatus is not null)
+        {
+            logger.LogInformation("Next '{functionName}' call scheduled at: {time}", FunctionName, timer.ScheduleStatus.Next);
+        }
+    }
+}
--- src/WeightTracker.Functions/Program.cs
+++ src/WeightTracker.Functions/Program.cs
@@ -1,7 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WeightTracker.Functions.Notifications;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddScoped<INotificationService, NotificationService>();
+
+        services.Configure<NotificationOptions>(context.Configuration.GetSection(NotificationOptions.Position));
+    })
     .Build();
 
 host.Run();
