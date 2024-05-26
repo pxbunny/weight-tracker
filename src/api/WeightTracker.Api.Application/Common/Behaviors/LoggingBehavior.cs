b5b12131b90833ff6f547035831a168b4f9e67be
dae-ne <rafalcz29@gmail.com>
2024-05-26 16:16:45 +0200

    Add pipeline behaviors

---
+++ src/api/WeightTracker.Api.Application/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace WeightTracker.Api.Application.Common.Behaviors;
+
+/// <summary>
+/// Represents a logging behavior.
+/// </summary>
+/// <remarks>
+/// The logging behavior logs the request and response of the request handler.
+/// </remarks>
+/// <param name="logger">The logger.</param>
+/// <typeparam name="TRequest">The request type.</typeparam>
+/// <typeparam name="TResponse">The response type.</typeparam>
+internal sealed class LoggingBehavior<TRequest, TResponse>(ILogger<TRequest> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+
+        logger.LogInformation("Handling {RequestName}: {@Request}", requestName, request);
+
+        var response = await next();
+
+        logger.LogInformation("Handled {RequestName}: {@Request} | {@Response}", requestName, request, response);
+
+        return response;
+    }
+}
--- src/api/WeightTracker.Api.Application/DependencyInjection.cs
+++ src/api/WeightTracker.Api.Application/DependencyInjection.cs
@@ -1,10 +1,20 @@
 ﻿using Mapster;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using WeightTracker.Api.Application.Common.Behaviors;
 
 namespace WeightTracker.Api.Application;
 
+/// <summary>
+/// Contains the dependency injection configuration for the application layer.
+/// </summary>
 public static class DependencyInjection
 {
+    /// <summary>
+    /// Adds the application services to the specified service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         var assembly = typeof(DependencyInjection).Assembly;
@@ -14,6 +24,8 @@ public static class DependencyInjection
         services.AddMediatR(cfg =>
         {
             cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
         });
 
         return services;
