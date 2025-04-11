55712cf5112dd25119019eac05d91525388693a7
dae-ne <rafalcz29@gmail.com>
2025-04-11 14:10:27 +0200

    Update mapping in backend

--- server/WeightTracker.Api/Endpoints/Weight/Get/WeightGetEndpoint.cs
+++ server/WeightTracker.Api/Endpoints/Weight/Get/WeightGetEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-
-namespace WeightTracker.Api.Endpoints.Weight.Get;
+﻿namespace WeightTracker.Api.Endpoints.Weight.Get;
 
 public class WeightGetEndpoint : Endpoint<WeightGetRequest, WeightGetResponse>
 {
@@ -10,28 +8,8 @@ public class WeightGetEndpoint : Endpoint<WeightGetRequest, WeightGetResponse>
 
     public override async Task HandleAsync(WeightGetRequest request, CancellationToken ct)
     {
-        var (dateFromStr, dateToStr) = request;
-
-        var dateFrom = string.IsNullOrWhiteSpace(dateFromStr)
-            ? DateOnly.MinValue
-            : DateOnly.Parse(dateFromStr);
-
-        var dateTo = string.IsNullOrWhiteSpace(dateToStr)
-            ? DateOnly.MaxValue
-            : DateOnly.Parse(dateToStr);
-
-        var userId = CurrentUser.Id;
-
-        var query = new GetWeightDataQuery(userId, dateFrom, dateTo);
+        var query = request.ToQuery(CurrentUser.Id);
         var data = await query.ExecuteAsync(ct);
-
-        Response = new WeightGetResponse
-        {
-            UserId = data.UserId,
-            Avg = data.AverageWeight,
-            Max = data.MaxWeight,
-            Min = data.MinWeight,
-            Data = data.Data.Select(d => new WeightDataListItem(d.Date.ToFormattedString(), d.Weight))
-        };
+        Response = data.ToResponse();
     }
 }
--- server/WeightTracker.Core/DateOnlyExtensions.cs
+++ server/WeightTracker.Core/DateOnlyExtensions.cs
@@ -4,5 +4,5 @@ public static class DateOnlyExtensions
 {
     private const string DateFormat = "yyyy-MM-dd";
 
-    public static string ToFormattedString(this DateOnly date) => date.ToString(DateFormat); // TODO: change name
+    public static string ToDomainDateString(this DateOnly date) => date.ToString(DateFormat);
 }
