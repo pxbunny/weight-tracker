432ef1cd68e67443da0c7e21af2c775b3d29a5fb
dae-ne <rafalcz29@gmail.com>
2025-04-25 14:23:27 +0200

    Update variables in scripts

--- scripts/build_exe.sh
+++ scripts/build_exe.sh
@@ -6,16 +6,16 @@ root_dir=$(git rev-parse --show-toplevel)
 
 echo "Loading environment variables..."
 
-source "$root_dir"/.env
+source $root_dir/.env
 
 echo "Creating executable..."
 
-rm -rf "$root_dir"/client/dist
+rm -rf $root_dir/client/dist
 
-pyinstaller "$root_dir"/client/wtrack/__main__.py \
-  --name ${CLI_APP_NAME} \
-  --distpath "$root_dir"/client/dist \
-  --workpath "$root_dir"/client/build \
+pyinstaller $root_dir/client/wtrack/__main__.py \
+  --name $CLI_APP_NAME \
+  --distpath $root_dir/client/dist \
+  --workpath $root_dir/client/build \
   --log-level=WARN
 
 branch_name=$(git rev-parse --abbrev-ref HEAD)
@@ -32,5 +32,5 @@ fi
 
 echo "Copying executable to $CLI_APP_DIR..."
 
-cp "$root_dir"/client/config.prod.json "$root_dir"/client/dist/${CLI_APP_NAME}/config.json
-cp -r "$root_dir"/client/dist/${CLI_APP_NAME} $CLI_APP_DIR
+cp $root_dir/client/config.prod.json $root_dir/client/dist/$CLI_APP_NAME/config.json
+cp -r $root_dir/client/dist/$CLI_APP_NAME $CLI_APP_DIR
