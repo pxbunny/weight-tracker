b3d256cb748a9979fe2ce9f764be9c16f4d1755e
dae-ne <rafalcz29@gmail.com>
2025-11-07 20:02:37 +0100

    Fix status calculations

--- app-api/WeightTracker.Core/Models/Status.cs
+++ app-api/WeightTracker.Core/Models/Status.cs
@@ -3,26 +3,29 @@ using System.Linq;
 
 namespace WeightTracker.Core.Models;
 
-public record Status(bool AddedForToday, int MissedInLast7Days, int MissedInLast30Days)
+public sealed record Status(bool AddedForToday, int MissedInLast7Days, int MissedInLast30Days)
 {
-    public static Status GetStatus(IList<WeightData> data)
+    public static Status Empty => new(false, 0, 0);
+
+    public static Status GetStatus(IList<WeightData> data, DateOnly? referenceDate = null)
     {
         var lastDate = data.LastOrDefault()?.Date;
 
-        return lastDate is null
-            ? new Status(false, 0, 0)
-            : new Status(
-                lastDate == DateOnly.FromDateTime(DateTime.Today),
-                CalculateMissedDays(data, 7),
-                CalculateMissedDays(data, 30));
+        if (lastDate is null) return Empty;
+
+        var today = referenceDate ?? DateOnly.FromDateTime(DateTime.Today);
+        var isDataAddedForToday = data.Any(d => d.Date == today);
+        var missingInLast7Days = CalculateMissedDays(data, 7, today);
+        var missingInLast30Days = CalculateMissedDays(data, 30, today);
+        return new Status(isDataAddedForToday, missingInLast7Days, missingInLast30Days);
     }
 
-    private static int CalculateMissedDays(IEnumerable<WeightData> data, int totalDays)
+    private static int CalculateMissedDays(IEnumerable<WeightData> data, int totalDays, DateOnly? today = null)
     {
         if (totalDays <= 0) return 0;
 
-        var lastDate = DateOnly.FromDateTime(DateTime.Today);
-        var firstDate = lastDate.AddDays(-totalDays);
+        var lastDate = today ?? DateOnly.FromDateTime(DateTime.Today);
+        var firstDate = lastDate.AddDays(-totalDays + 1);
 
         var datesInRange = data
             .Select(d => d.Date)
